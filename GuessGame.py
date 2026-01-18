a8bda11ba614de361f6d62a5fb8ff0394c953535
prash3072004 <123569400+prash3072004@users.noreply.github.com>
2026-01-18 12:06:31 +0530

    Add files via upload

---
+++ GuessGame.py
@@ -0,0 +1,29 @@
+import random as rdm
+while True:
+    secretnumber = rdm.randint(0,100)
+    for i in range(3):
+        try:
+            userinput = int (input("Guess a value from 0 to 100: "))
+        except:
+            print('Invalid input.Please enter an integer value: ')
+            continue                                   
+        if userinput==secretnumber:
+            print('You won!')
+            break
+        else:
+            if i==2:
+                print('You lost.The correct guess was',secretnumber)
+            else:
+                print('Wrong guess.Try again.')
+    print('Do you wanna play again?')
+    choice = input('Y/N: ').upper()
+    if choice =='Y':
+        continue
+    elif choice =='N':
+        print('Thank you for playing. You have exited the game')
+        break
+    else :
+        print('Invalid input.You have exited game')
+        break
+
+            
